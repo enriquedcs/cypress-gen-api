59617ae186ecf4eff1389195ee570f667341b11c
Enrique Alejandro De Coss Martinez <enrique.decoss@kueski.com>
2022-05-26 20:25:37 -0500

    First commit Cypress

---
+++ cypress/integration/api.spec.js
@@ -0,0 +1,45 @@
+/// <reference types="Cypress" />
+
+import config from './config.json'
+
+describe('APi Test using Cypress', () => {
+    it('API - Validate Headers', () => {
+        cy.request({
+            method: 'GET',
+            url: `${config.URL}`,
+            failOnStatusCode: false,
+        }).as('poke')
+        cy.get('@poke')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+    })
+
+    it('API - Validate Status Code', () => {
+        cy.request({
+            method: 'GET',
+            url: `${config.URL}`,
+            failOnStatusCode: false,
+        }).as('poke')
+        cy.get('@poke').its('status').should('equal', 200)
+    })
+
+    it('API - Validate Content Values', () => {
+        cy.request({
+            method: 'GET',
+            url: `${config.URL}`,
+            failOnStatusCode: false,
+        }).as('poke')
+        cy.get('@poke').its('body').should('include', { name: 'pikachu' })
+    })
+
+    it('API - Validate Negative status code', () => {
+        cy.request({
+            method: 'GET',
+            url: `${config.URL2}`,
+            failOnStatusCode: false,
+        }).as('poke')
+        cy.get('@poke').its('status').should('equal', 404)
+    })
+
+})
